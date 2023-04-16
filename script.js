c41833fb013f40560101738d9478c852d1d9684c
M. Andres Vinces M <128201678+mavinces@users.noreply.github.com>
2023-04-16 01:32:00 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  const downloadButton = document.querySelector('.download');
-  downloadButton.addEventListener('click', function() {
-    window.location.href = downloadButton.href;
-  });
-}
